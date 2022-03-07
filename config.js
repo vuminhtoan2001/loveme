5827357f2c942ca709b5639da935c09ea535e46b
Vu Minh Toan <vuminhtoan2001@gmail.com>
2022-03-07 23:15:46 +0700

    Upload love stp1

--- config.js
+++ config.js
@@ -1,18 +1,19 @@
 // 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
 const CONFIG = {
-    introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
-    introDesc: `Anh có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha`,
-    btnIntro: 'OK',
-    title: 'Anh biết Em thích Anh phải không phải không ? 😙',
+    introTitle: ' Mùng 8/3 vui vẻ !️🎉💐',
+    introDesc: `Anh có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha!(Nghe nhạc chill rồi bấm từ từ cũng được.)`,
+    btnIntro: 'OKee 🤔',
+    title: 'Em có 1 tý gì thích Anh không? Có không ? 😙',
     desc: 'Em không trả lời hoặc thoát ra, tức là Em thích Anh đó nha :> ',
-    btnYes: 'Có, em thật sự thích anh 💖💖💖',
+    btnYes: '💖💖Có, em có thích anh 💖💖',
     btnNo: '💔 Anh mơ à ! 💔 ',
     question:'Nói cho Anh biết đi vì sao Em thích anh 😊😊',
-    reasonPlaceholder: 'Lý do',
+    reasonPlaceholder: 'Do không bấm được nút kia hay do 😍...',
     btnReply: 'Gởi ! 💌💌',
-    reply: 'Tại vì anh đẹp trai đó Ahiiiii 😊😊',
-    mess: 'Anh biết mà, Anh cũng thích em lắm 🥰. Love you 300.000 😘😘',
+    reply: 'Tại vì anh đẹp trai đã thế còn độc và lạ ahihi....................',
+    mess: 'Anh biết mà, Anh cũng thích em lắm 🥰. Love you <3:)(0) 😘😘',
     messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
-    btnAccept: 'Ok lun 💖',
-    messLink: 'https://m.me/PhungQuocDinh'
+    btnAccept: 'Ok lun 💘',
+    messLink: 'https://www.messenger.com/t/vu.minhtoan.98837/',
+    step2Link: 'https://vuminhtoan2001.github.io/lovemoon_step2/'
 }
--- js/main.js
+++ js/main.js
@@ -1,15 +1,96 @@
+var audio_datkaa = new Audio('sound/havuongnang.mp3');
+var audio_jukySan = new Audio('sound/sound.mp3');
+var audio_erik = new Audio('sound/erik-cukak-remix-audio-lyrics-video.mp3');
+var audio_justatee = new Audio('sound/da-lo-yeu-em-nhieu-official-mv.mp3');
 $(document).ready(function() {
-    // process bar
+    audio_erik.play();
     setTimeout(function() {
         firstQuestion();
         $('.spinner').fadeOut();
-        $('#preloader').delay(350).fadeOut('slow');
-        $('body').delay(350).css({
+        $('#preloader').delay(59000).fadeOut('slow');
+        $('body').delay(9950).css({
             'overflow': 'visible'
         });
-    }, 600);
+    }, 0);
+    setTimeout(typeWriter(),4000)
 })
+function loopQ1(){
+    setTimeout(()=>{
+        audio_datkaa.play();
+    },63000)
+    setTimeout(()=>{
+        document.getElementById("demo").style.display = "none";
+    },60000)
+    // 63000
+    Swal.fire({
+        title: CONFIG.introTitle,
+        text: CONFIG.introDesc,
 
+        // 2. Ảnh ở câu hỏi đầu tiên
+        imageUrl: 'https://raw.githubusercontent.com/zukahai/Confess-Crush/main/img/logi.gif',
+        imageWidth: 300,
+        imageHeight: 300,
+        background: `#fff url("https://cdn.dribbble.com/users/2286753/screenshots/10052587/love_background-01.jpg") 100% 100% `,
+        backdrop: `
+              rgba(0,0,123,0.4)
+              url("https://condaluna.com/assets/stickers/hearts-fireworks.gif")
+
+            `,
+        imageAlt: 'Custom image',
+        showCancelButton: true,
+        cancelButtonText: 'Đánh m bh hỏi hỏi 👊🥊',
+        confirmButtonText: CONFIG.btnIntro
+      }).then(function(result){
+        // audio_datkaa.pause();
+
+        // $('.content').show(200);
+        if (result.isConfirmed) {
+            setTimeout(()=>{
+                audio_datkaa.pause();
+            },0)
+            audio_jukySan.play()
+            $('.content').show(200);
+          } else {
+            loopQ2();
+          }
+        secondQuestion();
+      })
+}
+function loopQ2(){
+    $('.spinner').fadeIn();
+    $('#preloader').delay(500).fadeIn('slow');
+    $('.spinner').fadeOut();
+    $('#preloader').delay(5000).fadeOut('slow');
+    Swal.fire({
+        title: "He he mất nút kia rồi còn một nút này thôi! 😹",
+        text: CONFIG.introDesc,
+
+        // 2. Ảnh ở câu hỏi đầu tiên
+        imageUrl: 'https://raw.githubusercontent.com/zukahai/Confess-Crush/main/img/logi.gif',
+        imageWidth: 300,
+        imageHeight: 300,
+        background: `#fff url("https://cdn.dribbble.com/users/2286753/screenshots/10052587/love_background-01.jpg") 100% 100% `,
+        backdrop: `
+              rgba(0,0,123,0.4)
+              url("https://condaluna.com/assets/stickers/hearts-fireworks.gif")
+
+            `,
+        imageAlt: 'Custom image',
+        denyButtonText: `Don't save`,
+        confirmButtonText: CONFIG.btnIntro
+      }).then(function(result){
+        if (result.isConfirmed) {
+            setTimeout(()=>{
+                audio_datkaa.pause();
+            },0)
+            audio_jukySan.play();
+            $('.content').show(200);
+          } else {
+            loopQ2();
+          }
+        secondQuestion();
+      })
+}
 function init(){
     $('#title').text(CONFIG.title)
     $('#desc').text(CONFIG.desc)
@@ -19,20 +100,70 @@ function init(){
 
 function firstQuestion(){
     $('.content').hide();
-    Swal.fire({
-        title: CONFIG.introTitle,
-        text: CONFIG.introDesc,
+    loopQ1();
 
-        // 2. Ảnh ở câu hỏi đầu tiên
-        imageUrl: 'img/cuteCat.jpg',
-        imageWidth: 300,
-        imageHeight: 300,
-        background: '#fff url("img/iput-bg.jpg")',
-        imageAlt: 'Custom image',
-        confirmButtonText: CONFIG.btnIntro
-      }).then(function(){
-        $('.content').show(200);
-      })
+}
+function secondQuestion(){
+    // audio_jukySan.play();
+$('#yes').click(function() {
+    var audio = new Audio('sound/tick.mp3');
+    audio.play();
+    audio_jukySan.play();
+    Swal.fire({
+        title: CONFIG.question,
+        text: 'Do không bấm được nút kia hay là do:',
+        html: true,
+        width: 900,
+        padding: '3em',
+        html: "<input type='text' class='form-control' id='txtReason' onmousemove=textGenerate()  placeholder='" + CONFIG.reasonPlaceholder + "'>",
+        background: `#fff url("https://data.whicdn.com/images/353109914/original.gif")    center center` ,
+        backdrop: `
+              rgba(0,0,123,0.4)
+              url("https://bestanimations.com/uploads/gifs/1697162351rotating-red-heart-gif-animation61.gif")
+
+               top center
+              repeat
+            `,
+        confirmButtonColor: '#3085d6',
+        confirmButtonColor: '#fe8a71',
+        confirmButtonText: CONFIG.btnReply
+        }).then((result) => {
+            if (result.value) {
+                setTimeout(()=>{
+                    audio_jukySan.pause();
+                },0)
+                audio_justatee.play();
+                Swal.fire({
+                    width: 900,
+                    confirmButtonText: CONFIG.btnAccept,
+                    background: '#fff url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlEemuTNZE7w8pIMrGqW1rncqpE8sVwHR-Cw&usqp=CAU")',
+                    title: CONFIG.mess,
+                    text: CONFIG.messDesc,
+                    confirmButtonColor: '#83d0c9',
+                    backdrop: `
+                    rgba(0,0,123,0.4)
+                    url("https://cdn.dribbble.com/users/31664/screenshots/4117469/dribbble_2.gif")
+
+                    top center
+                    repeat
+                    `,
+                    showCancelButton: true,
+                    cancelButtonText: 'Chưa đủ đô thì tiếp nào.',
+                    cancelButtonColor: '#d33',
+                    onClose: () => {
+                        window.location = CONFIG.messLink;
+                    }
+                }).then(function(result){
+                    if (result.isConfirmed) {
+                        window.location = CONFIG.messLink;
+                      } else {
+                        window.location = CONFIG.step2Link;
+                      }
+                  })
+            }
+        })
+    })
+////Yes
 }
 
  // switch button position
@@ -95,41 +226,5 @@ function textGenerate() {
     $('#txtReason').val(n);
     setTimeout("textGenerate()", 1);
 }
-
-// show popup
-$('#yes').click(function() {
-    var audio = new Audio('sound/tick.mp3');
-    audio.play();
-    Swal.fire({
-        title: CONFIG.question,
-        html: true,
-        width: 900,
-        padding: '3em',
-        html: "<input type='text' class='form-control' id='txtReason' onmousemove=textGenerate()  placeholder='" + CONFIG.reasonPlaceholder + "'>",
-        background: '#fff url("img/iput-bg.jpg")',
-        backdrop: `
-              rgba(0,0,123,0.4)
-              url("img/giphy2.gif")
-              left top
-              no-repeat
-            `,
-        confirmButtonColor: '#3085d6',
-        confirmButtonColor: '#fe8a71',
-        confirmButtonText: CONFIG.btnReply
-    }).then((result) => {
-        if (result.value) {
-            Swal.fire({
-                width: 900,
-                confirmButtonText: CONFIG.btnAccept,
-                background: '#fff url("img/iput-bg.jpg")',
-                title: CONFIG.mess,
-                text: CONFIG.messDesc,
-                confirmButtonColor: '#83d0c9',
-                onClose: () => {
-                    window.location = CONFIG.messLink;
-                  }
-            })
-        }
-    })
-})
+// countdown
 
